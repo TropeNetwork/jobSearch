b9d8dc5fd09c89fe7c5bafd65a7a541f97b8e1d1
cbleek <cbleek>
2003-03-13 18:33:43 +0000

    insert data into search DB

--- lib/SearchIndex.php
+++ lib/SearchIndex.php
@@ -5,6 +5,15 @@
  * very simple search Server based on MySQL
  **/
 
+/**
+ * definitions from Job.class. This should propably move
+ * into a shared file. 
+ */
+define("JOB_STATUS_OFFLINE"            ,1);
+define("JOB_STATUS_OFFLINE_REQUESTED"  ,2);
+define("JOB_STATUS_ONLINE"             ,3);
+define("JOB_STATUS_ONLINE_REQUESTED"   ,4);
+
 require_once(OPENHR_LIB."/Database.php");
 
 class SearchIndex {
@@ -16,11 +25,25 @@ class SearchIndex {
     }
 
     function insert($key,$data){
-        return "INSERT $key";
+
+        $query="REPLACE 
+                   INTO jobs 
+                        (job_id, job_title, location, employer, incoming_date)
+                 VALUES (?,?,?,?,now())";     
+        $sth=$this->db->prepare($query);
+        $this->db->execute($sth,array($key,$data["job_title"],$data["location"],""));
+
+        return array("key"=>$key,
+                     "action"=> JOB_STATUS_ONLINE_REQUESTED,
+                     "return"=> array("errno"  => 0,
+                                      "errstr" => "OK"));
     }
     
     function delete($key){
-        return "DELETE $key";
+        return array("key"=>$key,
+                     "action"=> JOB_STATUS_OFFLINE_REQUESTED,
+                     "return"=> array("errno"  => 0,
+                                      "errstr" => "OK"));
     }
 
 }
